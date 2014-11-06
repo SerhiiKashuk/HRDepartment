1c6c25edc9052e410b79ad4cc0a429f4bf954700
sergiolaza <kashuk.sergii@gmail.com>
2014-11-06 11:36:01 +0200

    Update Function.java

--- HRDep/src/resourse/Function.java
+++ HRDep/src/resourse/Function.java
@@ -2,22 +2,17 @@ package resourse;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.io.FileReader;
 import java.io.IOException;
-
-
-import java.util.ArrayList;
-import java.util.Iterator;
 
 import resourse.HR;
 
 public class Function {
-	
+
 	private static BufferedReader br;
-	
-	
-	public static  String getLastRecord(File searchFile) throws IOException {
+
+	public static String getLastRecord(File searchFile) throws IOException {
 		br = new BufferedReader(new FileReader(searchFile));
 		String current = null;
 		String last = null;
@@ -29,53 +24,51 @@ public class Function {
 		return last;
 
 	}
-	
-	public static String getValueByPosition(String res,int position, String sep){
-		
-		String[]record = res.split(sep);
-		
+
+	public static String getValueByPosition(String res, int position, String sep) {
+
+		String[] record = res.split(sep);
+
 		return record[position];
 	}
-	
+
 	public static int getLastId(File searchFile) throws IOException {
 
 		int lastId = -1;
 		if (searchFile.length() != 0) {
 
 			String lRec = Function.getLastRecord(searchFile);
-			
-			lastId = Integer.valueOf(Function.getValueByPosition
-					(lRec, 0, HR.getSeparator()));
+
+			lastId = Integer.valueOf(Function.getValueByPosition(lRec, 0, HR.getSeparator()));
 		}
 		return lastId;
 	}
-	
-	public static String getRecordById(File searchFile, int id,String sep) throws IOException{
-		
-		String result = null;
-		String current = null;
-	
-		br = new BufferedReader(new FileReader(searchFile));
 
-		while ((current = br.readLine()) != null) {
-			if(id == Integer.valueOf(Function.getValueByPosition(current, 0,sep))){
-				result = current;
+	public static String getRecordById(File searchFile, int id) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(searchFile));
+
+		boolean foundFlag = false;
+
+		String currRec = br.readLine();
+
+		while (currRec != null) {
+
+			int currId = Integer.valueOf(getValueByPosition(currRec, 0, HR.getSeparator()));
+
+			if (currId == id) {
+				foundFlag = true;
+				break;
 			}
-			
 
+			currRec = br.readLine();
 		}
+
 		br.close();
-		
-	
-		return result;
-	}
-	public static String insertValue() {
-		String result = null;
-		
-		return result;
-	}
 
-	
+		if (!foundFlag) {
+			System.out.println("Enter other ID");
+		}
+
+		return currRec;
+	}
 }
-
-
