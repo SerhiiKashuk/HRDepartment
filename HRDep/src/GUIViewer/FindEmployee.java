26c2981d33cc6dbdf5085a22588f9ca8d4956c9a
sergiolaza <kashuk.sergii@gmail.com>
2014-11-06 11:41:46 +0200

    Update FindEmployee.java

--- HRDep/src/GUIViewer/FindEmployee.java
+++ HRDep/src/GUIViewer/FindEmployee.java
@@ -1,66 +1,52 @@
 package GUIViewer;
 
-
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JTextField;
-import javax.swing.JEditorPane;
-import javax.swing.JButton;
-
-import resourse.Function;
-import resourse.HR;
 
 public class FindEmployee extends JFrame {
 
+	private static final long serialVersionUID = 23456789;
 	private JPanel contentPane;
 	private JTextField idEmployee;
 	private JButton res;
 
 	public FindEmployee() {
+		setTitle("Search Result");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 390, 273);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel id = new JLabel("ID");
 		id.setHorizontalAlignment(SwingConstants.CENTER);
 		id.setBounds(63, 33, 46, 44);
 		contentPane.add(id);
-		
+
 		idEmployee = new JTextField();
-		idEmployee.setBounds(132, 45, 86, 20);
+		idEmployee.setBounds(129, 45, 89, 20);
 		contentPane.add(idEmployee);
 		idEmployee.setColumns(10);
-		
+
 		final JEditorPane result = new JEditorPane();
-		result.setBounds(82, 136, 212, 83);
+		result.setBounds(10, 136, 354, 60);
 		contentPane.add(result);
-		
+
 		res = new JButton("Result");
-		res.setBounds(129, 92, 89, 23);
+		res.setBounds(129, 90, 89, 23);
 		contentPane.add(res);
 		res.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
+
 				HR human = HR.getHR();
 				try {
-					
-					String record = Function.getRecordById
-					           (human.getStorage(), Integer.valueOf(idEmployee.getText()));
+
+					String record = Function.getRecordById(human.getStorage(), Integer.valueOf(idEmployee.getText()));
 					result.setText(record);
 				} catch (NumberFormatException | IOException e1) {
-					// TODO Auto-generated catch block
+
 					e1.printStackTrace();
 				}
 			}
