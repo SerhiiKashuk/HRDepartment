0095cbad09e099aed7a5c9ef9c5ba6531d2aab82
sergiolaza <kashuk.sergii@gmail.com>
2014-11-06 11:42:40 +0200

    Update ChangeValues.java

--- HRDep/src/GUIViewer/ChangeValues.java
+++ HRDep/src/GUIViewer/ChangeValues.java
@@ -1,60 +1,40 @@
+package GUIViewer;
 
-	package GUIViewer;
-
-	import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-	import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
+public class ChangeValues extends JFrame {
 
-import resourse.HR;
+	private static final long serialVersionUID = 54356785676L;
+	private JPanel contentPane;
+	private JTextField insertValue;
 
-	public class ChangeValues extends JFrame {
+	public ChangeValues() {
+		setTitle("Reconfigure");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(400, 400, 350, 201);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
-		private JPanel contentPane;
-		private JTextField insertValue;
-		private static String res;
-		
+		insertValue = new JTextField();
+		insertValue.setBounds(71, 45, 176, 20);
+		contentPane.add(insertValue);
+		insertValue.setColumns(10);
 
-		
+		JButton okKey = new JButton("OK");
+		okKey.setBounds(132, 113, 53, 23);
+		contentPane.add(okKey);
+		okKey.addActionListener(new ActionListener() {
 
-		public ChangeValues() {
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setBounds(400, 400, 350, 201);
-			contentPane = new JPanel();
-			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-			setContentPane(contentPane);
-			contentPane.setLayout(null);
-			
-			insertValue = new JTextField();
-			insertValue.setBounds(74, 72, 176, 20);
-			contentPane.add(insertValue);
-			insertValue.setColumns(10);
-			
-			JButton okKey = new JButton("OK");
-			okKey.setBounds(132, 113, 53, 23);
-			contentPane.add(okKey);
-			okKey.addActionListener(new ActionListener() {
-				
-		
-				public void actionPerformed(ActionEvent e) {
-					String res = insertValue.getText();
-					HR.setInsertVal(res);
-					setVisible(false);
-					
-				}
-			});
-		     
-		}
+			public void actionPerformed(ActionEvent e) {
+				String res = insertValue.getText();
+				HR.setInsertVal(res);
+				setVisible(false);
 
-
-
+			}
+		});
 
 	}
 
+}
